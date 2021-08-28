6c0ed40e4a9f3a0a9019fa38ebae5628441e8881
Diego Fischer <diego@carupi.com>
2021-08-28 02:59:58 -0300

    evoluindo

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,25 @@ import React from 'react'
 import { Container } from '@material-ui/core'
 import { Card } from '@material-ui/core'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function Body() {
   const api_key = 'BdgQlWf6ucvJktrCP2SxWEKf1fnAUcT4GXx4tIFs'
+  const [obj, setObj] = useState(null)
 
-  const fetchData = async () => {
-    const { data } = await axios.get('https://api.nasa.gov/planetary/apod', {
-      params: {
-        api_key,
-      },
-    })
-    return data
-  }
-
-  fetchData().then((data) => console.log(data))
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod', {
+        params: {
+          api_key,
+        },
+      })
+      .then((r) => setObj(r.data))
+  })
 
   return (
     <Container>
-      <Card>Hello</Card>
+      <Card>{JSON.stringify(obj)}</Card>
     </Container>
   )
 }
